22d32f9d44ffbfa0abaf46b590d9ede25eee0b87
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-03-31 14:18:38 +0200

    Implémentation d'une classe pour les notes

--- APP2/scripts/utils.py
+++ APP2/scripts/utils.py
@@ -1,9 +1,19 @@
 import numpy as np
 
 class Notes:
+    MAXFREQ = 5000
     def __init__(self):
         self.notes = np.array([32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74])
         self.names = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
+
+    def isValid(self):
+        return self.notes[-1] < Notes.MAXFREQ
+    
+    def items(self):
+        return zip(self.names, self.notes)
+    
+    def reset(self):
+        self.notes = np.array([32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74])
 
     def up(self):
         self.notes = 2 * self.notes
