1e8597eea24ad230d872222b0beac6922822f9cc
Raphael Dalle Risbourg <raphael.dalle@telecom-paris.fr>
2025-04-07 10:33:36 +0200

    Maj pour avoir les instruments

--- APP2/scripts/bruteforce.py
+++ APP2/scripts/bruteforce.py
@@ -4,7 +4,10 @@ from scipy.signal import stft
 import soundfile as sf
 import numpy as np
 
-TRESHOLD = 0.9
+VERBOSE = False
+TRESHOLD_NOTE = 0.9
+TRESHOLD_INSTRUMENT = 0.5
+TRESHOLD_BOTH = 0.1
 EPSILON = 1e-9
 REP = "audio/"
 EXT = ".wav"
@@ -34,14 +37,21 @@ def analyseNoteRaphael(file):
 
     n = len(reference.notes)
     m = time.shape[0]
+    
+    
     history = np.zeros((m, n), dtype=np.uint)
+    instruments = np.zeros((m, 2)) #0: Guitare, 1: Percussions
+    former = [] #Energie max à chaque instant
     ratios = []
-    print("Nombre de steps: ", m)
+    if VERBOSE:
+        print("Nombre de steps: ", m)
 
     for t in range(m):
         
-        fft = np.abs(Zxx[:, t])
-        print("Instant: ", time[t])
+        fft = np.abs(Zxx[:, t]) # Fft à l'instant time[t]
+        if VERBOSE:
+            print("Instant: ", time[t])
+
         # On détermine la note
         reference.reset()
         energy = {name: 0 for name in reference.names}
@@ -52,19 +62,44 @@ def analyseNoteRaphael(file):
             reference.up()
 
         e_max = max(energy.values())
+        former.append(e_max) # On enregistre l'energie
+
         for index, name in enumerate(reference.names):
-            if energy[name]/e_max > TRESHOLD:
+            if energy[name]/e_max > TRESHOLD_NOTE:
                 history[t][index] = 1
-                print(f"Note: {name} ; Energy: {energy[name]}")
-    
+                if VERBOSE:
+                    print(f"Note: {name} ; Energy: {energy[name]}")
+
         # On détermine l'instrument
         mask = (100 <= frequences) & (frequences < 5000)
         sample = fft[mask]
         arithmetic_mean = np.mean(sample)
         geometric_mean = np.exp(np.mean(np.log(sample + EPSILON)))
-        ratios.append(geometric_mean / (arithmetic_mean + EPSILON))
+        ratio = geometric_mean / (arithmetic_mean + EPSILON)
+        ratios.append(ratio)
 
-    plt.plot(time, ratios)
+        if ratio > TRESHOLD_INSTRUMENT + TRESHOLD_BOTH:
+            instruments[t][1] = 1
+        elif ratio < TRESHOLD_INSTRUMENT - TRESHOLD_BOTH:
+            instruments[t][0] = 1
+        else: 
+            instruments[t][0] = 1
+            instruments[t][1] = 1
+
+
+    # On annule les valeurs dont l'energie est trop faible
+    threshold = np.mean(former) 
+    for t in range(m):
+        if former[t] <= threshold:
+            ratios[t] = 0
+            instruments[t][0] = 0
+            instruments[t][1] = 0
+            for i in range(len(reference.names)):
+                history[t][i] = 0
+
+    plt.plot(time, ratios, color="black")
+    plt.plot((time[0], time[-1]), (TRESHOLD_INSTRUMENT-TRESHOLD_BOTH, TRESHOLD_INSTRUMENT-TRESHOLD_BOTH), color="red")
+    plt.plot((time[0], time[-1]), (TRESHOLD_INSTRUMENT+TRESHOLD_BOTH, TRESHOLD_INSTRUMENT+TRESHOLD_BOTH), color="red")
     plt.xlabel("Time")
     plt.ylabel("Ratio")
     plt.ylim(0, 1)
